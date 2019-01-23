6ea98fad0dc26ce382cc235b8e3fa79eae07ef58
claire <github email address>
2019-01-23 13:57:45 -0600

    changed seed file, added relationships

--- app/controllers/quotes_controller.rb
+++ app/controllers/quotes_controller.rb
@@ -1,2 +1,15 @@
 class QuotesController < ApplicationController
+
+  
+    
+    def index
+        render json: Dragon.all
+    end
+    
+    # def show
+    #     render json: current_dragon, include: [ :abilities ]
+    # end
+    
+    
+
 end
--- app/models/player.rb
+++ app/models/player.rb
@@ -1,2 +1,4 @@
 class Player < ApplicationRecord
+    has_many :quotes
+    has_many :rounds, through: :quotes
 end
